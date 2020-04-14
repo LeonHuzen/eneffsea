af35b4d1d1be8057a3b4d6da01da40e60b7571f5
Leon Huzen <leon.huzen@noordhoff.nl>
2020-04-14 09:27:41 +0200

    add cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [hasNFC, setHasNFC] = useState();
@@ -10,10 +10,18 @@ function App() {
   useEffect(() => {
     const scanButton = document.getElementById("scanButton");
     const writeButton = document.getElementById("writeButton");
-    const log = (message) =>
+    const log = (message) => {
+      console.log(message);
       document
         .getElementById("log")
-        ?.appendChild(document.createTextNode(`${message} \n`));
+        .appendChild(document.createTextNode(`${message} \n`));
+    };
+    var cards = document.querySelectorAll(".card");
+    cards.forEach((card) =>
+      card.addEventListener("click", function () {
+        card.classList.toggle("is-flipped");
+      })
+    );
     scanButton.addEventListener("click", async () => {
       log("User clicked scan button");
 
@@ -29,41 +37,59 @@ function App() {
         reader.addEventListener("reading", ({ message, serialNumber }) => {
           log(`> Serial Number: ${serialNumber}`);
           log(`> Records: (${message.records.length})`);
-          const { records } = message;
-          records.forEach(record => {
-            log(`>> ${JSON.parse(record).toString()}`)
-          });
+          // const { records } = message;
+          // records.forEach((record) => {
+          //   log(`>> ${JSON.parse(record).toString()}`);
+          // });
         });
       } catch (error) {
         log("Argh! Scan Error " + error);
       }
     });
 
-    writeButton.addEventListener("click", async () => {
-      log("User clicked write button");
+    // writeButton.addEventListener("click", async () => {
+    //   log("User clicked write button");
 
-      try {
-        const writer = new NDEFWriter();
-        await writer.write("Hello world!");
-        log("> Message written");
-      } catch (error) {
-        log("Argh! " + error);
-      }
-    });
+    //   try {
+    //     const writer = new NDEFWriter();
+    //     await writer.write("Hello world!");
+    //     log("> Message written");
+    //   } catch (error) {
+    //     log("Argh! " + error);
+    //   }
+    // });
   }, [NFCLog, hasNFC, setNFCLog]);
   return (
     <div className={`App ${hasNFC ? "has-nfc" : ""}`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button id="scanButton">Scan</button>
-        <button id="writeButton">Write</button>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <button id="scanButton">Start the game</button>
+        {/* <button id="writeButton">Write</button> */}
         <p id="log" style={{ maxWidth: "100%" }}></p>
-        <button
+        <div>
+          <div class="scene scene--card">
+            <div class="card">
+              <div class="card__face card__face--front kans">Chance</div>
+              <div class="card__face card__face--back kans">Go to jail.</div>
+            </div>
+          </div>
+          <div class="scene scene--card">
+            <div class="card">
+              <div class="card__face card__face--front algemeen-fonds">
+                Community chest
+              </div>
+              <div class="card__face card__face--back algemeen-fonds">
+                Get out of jail, free.
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <button
           className="App-link"
           onClick={() => window.location.reload(true)}
         >
           Refresh now
-        </button>
+        </button> */}
         <ul>
           {NFCLog.map((entry, index) => (
             <li key={index}>{entry}</li>
